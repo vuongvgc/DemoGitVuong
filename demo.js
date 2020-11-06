f45fcee5776804c1198d28b0fd6d12b4cafcb029
Nguyen <tranchauthanhnguyen@gmail.com>
2020-11-06 19:51:00 +0700

    add new code

--- demo.js
+++ demo.js
@@ -2,3 +2,5 @@ console.log("create project");
 // step 1 : git init => create project 
 // step 2: add
 // step 3: login set  name and email để biết người nào commit code
+
+console.log("code of dev Nguyen");
