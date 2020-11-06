04fe94cc254437fc42f24af2d8983f684e5b207f
vuongvgc <vuongvgc@gmail.com>
2020-11-06 21:15:43 +0700

    commit brand Vuong

--- demo.js
+++ demo.js
@@ -3,5 +3,7 @@ console.log("create project");
 // step 2: add
 // step 3: login set  name and email để biết người nào commit code
 
+console.log("code moi nè");
 console.log("code of dev Nguyen - da chinh noi dung");
-console.log("code new  Branch Vuong")
+console.log("code of new branch");
+console.log("Da chinh Sua Code o Brand Vuong")
